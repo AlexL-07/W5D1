6ac9cd7f52dd232d4a8b212aa64562ac01f85345
AA Student <appacademy@Mini-46.local>
2022-10-31 16:45:09 -0700

    Finished phase 3

--- skeleton 2/lib/p03_hash_set.rb
+++ skeleton 2/lib/p03_hash_set.rb
@@ -4,21 +4,37 @@ class HashSet
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @set = Set.new({})
   end
 
   def insert(key)
+    if !include?(key)
+      @set.add(key)
+      self[key].push(key)
+      @count += 1
+      if @store.length == @count
+        self.resize!
+      end
+    end
   end
 
   def include?(key)
+    self[key].include?(key) 
   end
 
   def remove(key)
+    if include?(key)
+      @set.delete(key) 
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +42,9 @@ class HashSet
   end
 
   def resize!
+    @store = Array.new(@store.length * 2) { Array.new }
+    @set.each do |num|
+      self[num] << num
+    end 
   end
 end
